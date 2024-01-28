8a354bd41bf2845f31dfc06e1bfd2601cb4c2ea9
Yulia-1312 <140712549+Yulia-1312@users.noreply.github.com>
2024-01-28 21:03:07 +0100

    Added AI API to the project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#ingredients", {
-    strings: ` 2 pounds carrots (15 to 20 small to medium), 1 small yellow onion, 3
-            cloves garlic, 2 to 4 sprigs fresh dill, 1 medium lemon, 3 tablespoons
-            unsalted butter, 1 1/2 teaspoons kosher salt, 1/4 teaspoon freshly
-            ground black pepper, plus more for serving, 1 (32-ounce) carton
-            low-sodium vegetable or chicken broth (4 cups), 1/2 cup heavy cream
-            (optional) `,
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+function generateRecipe(event) {
+  event.preventDefault();
+  let ingredientsForm = document.querySelector("#ingredients");
+  ingredientsForm.innerHTML =
+    "Let me think what a recipe i have for you🤔Wait a little bit...";
+  let instructionInput = document.querySelector("#user-instruction");
+  let apiKey = "dad25fdb15t0a6cd0337444a00580o2f";
+  let prompt = `User request is: Generate a recipe with this product ${instructionInput.value}`;
+  let context =
+    "You are a very good cooker of simple food recipes and you like to cook fast and tasty. Your mission is to advice people in which simple recipe they can use their food. Please write in basic HTML. Make sure to follow user request.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayRecipe);
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
